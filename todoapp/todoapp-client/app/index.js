51a0e0c359d79d69dc9345f948443628142cf94f
Terry Sahaidak <s_tasssik_taras@ukr.net>
2015-10-09 23:20:34 +0300

    Hello world

---
+++ todoapp/todoapp-client/app/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {TodoApp} from './components';
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(<TodoApp />, rootElement);
